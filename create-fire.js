2d1ee7c1887a4bae14d2b1e13cf9217354a02a25
JimLynchCodes <mrdotjim@gmail.com>
2021-12-26 19:55:30 -0500

    initial scaffold

---
+++ create-fire.js
@@ -0,0 +1,8 @@
+
+async function createFire() {
+
+    // TODO create and fund a superfluid flow for this fire
+
+}
+
+export default createFire;   
---
+++ start-or-stop-spraying.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @param {boolean} stopSpraying 
+ */
+async function startOrStopSpraying(stopSpraying) {
+
+    // TODO calculate new amount to flow to hydrant owner
+    
+    // TODO edit flow for hydrant owner (possobly all hydrants spraying the fire?)
+
+}
+
+export default startSpraying;   
